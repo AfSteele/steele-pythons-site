2719e04c46d43f28a7213623461671982692ca36
AfSteele <asteele@121g.io>
2026-06-17 08:01:28 -0400

    convert to FTP format - static astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,12 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
-import astroAws from "@astro-aws/adapter";
 
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
   integrations: [react()],
-  output: "server",
-  adapter: astroAws({
-    mode: "ssr-stream",
-  }),
   image: {
     domains: ["steelepythons4.wordpress.com"],
   },
